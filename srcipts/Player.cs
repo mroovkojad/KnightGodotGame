2bb849ea521d92766ff4a38a5a7731473009351e
Michał Wojewoda <michal.wojewoda@hotmail.com>
2025-02-08 21:52:10 +0100

    Flip player sprite on movement 57:45

--- srcipts/Player.cs
+++ srcipts/Player.cs
@@ -1,10 +1,30 @@
 using Godot;
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 public partial class Player : CharacterBody2D
 {
 	public const float Speed = 130.0f;
 	public const float JumpVelocity = -300.0f;
+	AnimatedSprite2D animatedSprite;
+	public override void _Ready()
+	{
+		animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
+
+	public void flipSprite(Vector2 velocity, AnimatedSprite2D animatedSprite){
+		switch (velocity.X)
+		{
+			case float x when x > 0:
+				animatedSprite.FlipH = false;
+				break;
+			case float x when x < 0:
+				animatedSprite.FlipH = true;
+				break;
+			default:
+				break;
+		}
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -33,6 +53,8 @@ public partial class Player : CharacterBody2D
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 		}
+
+		flipSprite(velocity, animatedSprite);
 
 		Velocity = velocity;
 		MoveAndSlide();
