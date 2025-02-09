256529e16ef9f3b16ca6dbcd5cd7a04dc654eab1
Michał Wojewoda <michal.wojewoda@hotmail.com>
2025-02-09 11:53:48 +0100

    Remove killzonte console output

--- srcipts/killzone.cs
+++ srcipts/killzone.cs
@@ -10,11 +10,9 @@ public partial class killzone : Area2D
 		timer = GetNode<Timer>("Timer");
 		timer.Timeout += OnTimerTimeout;
 		Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
-		GD.Print("Killzone ready");
 	}
 	public void OnBodyEntered(Node2D body)
 	{
-		GD.Print("You died!");
 		Engine.TimeScale = 0.5f;
 		body.GetNode("CollisionShape2D").QueueFree();
 		timer.Start();
