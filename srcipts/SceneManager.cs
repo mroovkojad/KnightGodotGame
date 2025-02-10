ed02c62878e0501f2a3819d899ef5f59ffc4dd90
Michał Wojewoda <michal.wojewoda@hotmail.com>
2025-02-10 23:10:08 +0100

    Remove new lines in SceneManager

--- srcipts/SceneManager.cs
+++ srcipts/SceneManager.cs
@@ -1,7 +1,6 @@
 using Godot;
 using System;
 using System.Collections.Generic;
-
 // Enum to represent the main scenes of the game, that can be used as main view.
 public enum MainScenes
     {
@@ -11,18 +10,14 @@ public enum MainScenes
     }
 public partial class SceneManager : Node
 {
-
     // Dictionary to store the paths of the main scenes.
     Dictionary<MainScenes, string> ScenePaths = new Dictionary<MainScenes, string> {
     { MainScenes.MainMenu, "res://Scenes/Menu/MainMenu.tscn" },
     { MainScenes.Game, "res://Scenes/Game.tscn" },
     { MainScenes.Options, "res://Scenes/Menu/OptionsMenu.tscn" },
 };
-
     public Node CurrentScene { get; set; }
-
     public static SceneManager instance;
-
 
     public override void _Ready()
     {
@@ -30,12 +25,10 @@ public partial class SceneManager : Node
         CurrentScene = root.GetChild(-1);
         instance = this;
     }
-
     public  void ChangeScene(MainScenes sceneEnum)
     {
         CallDeferred(MethodName.DeferredChangeScene,  Variant.From(sceneEnum));
     }
-
     public  void DeferredChangeScene(MainScenes sceneEnum)
     {
         // It is now safe to remove the current scene.
@@ -47,21 +40,15 @@ public partial class SceneManager : Node
         {
             GD.PrintErr("Error freeing current scene: " + ex.Message);
         }
-
         // Get the path to the scene from the dictionary.
         string path = ScenePaths[sceneEnum];
-
         // Load a new scene.
         var nextScene = GD.Load<PackedScene>(path);
-
         // Instance the new scene.
         CurrentScene = nextScene.Instantiate();
-
         // Add it to the active scene, as child of root.
         GetTree().Root.AddChild(CurrentScene);
-
         // Optionally, to make it compatible with the SceneTree.change_scene_to_file() API.
         GetTree().CurrentScene = CurrentScene;
         }
-
 }
