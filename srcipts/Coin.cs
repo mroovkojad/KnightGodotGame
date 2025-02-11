3cdfccb1968cd0229ecf0eec8c23f4dacd4f05b3
Michał Wojewoda <michal.wojewoda@hotmail.com>
2025-02-11 19:50:39 +0100

    Add coin pickup sound and animation 1:14:15

--- srcipts/Coin.cs
+++ srcipts/Coin.cs
@@ -4,13 +4,15 @@ using System;
 public partial class Coin : Area2D
 {
 	Stats stats;
+	AnimationPlayer animationPlayer;
 	public override void _Ready()
 	{
 		stats = GetNode<Stats>("%Stats");
 		Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
+		animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 	}
 	public void OnBodyEntered(Node2D body){
 		stats.AddCoin();
-		QueueFree();
+		animationPlayer.Play("pickup");
 	}
 }
