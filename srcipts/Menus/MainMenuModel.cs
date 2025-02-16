46409aeaa2fd5da58e5d448c2fedf67d93d8d568
Michał Wojewoda <michal.wojewoda@hotmail.com>
2025-02-16 20:03:17 +0100

    Remove Main Menu MVC

--- srcipts/Menus/MainMenuModel.cs
+++
@@ -1,9 +0,0 @@
-public class MainMenuModel{
-    public string[] Options = {"Start", "Options", "ExitGame"};
-    public int SelectedIndex { get; private set; } = 0;
-
-    public void SelectOption(int index){
-        SelectedIndex = index;
-    }
-
-}
