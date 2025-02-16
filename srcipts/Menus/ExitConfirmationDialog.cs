30400aa5f10960948cefc3577e5330cf2ad8f910
Michał Wojewoda <michal.wojewoda@hotmail.com>
2025-02-16 20:04:49 +0100

    Fix Exit confirmation menu Sizes

--- srcipts/Menus/ExitConfirmationDialog.cs
+++ srcipts/Menus/ExitConfirmationDialog.cs
@@ -8,12 +8,14 @@ public partial class ExitConfirmationDialog : ConfirmationDialog
     public override void _Ready()
     {
         doNotAskAgainCheckBox = new CheckBox();
-      //  doNotAskAgainCheckBox.Theme = new ThemeManager().GetTheme("MinorTextTheme");
         Theme theme = new Theme();
-        theme.SetFontSize("font_size", "CheckBox", 8);
-        doNotAskAgainCheckBox.Theme = theme;
+        theme.SetFontSize("font_size", "CheckBox", 16);
+        theme.SetFontSize("font_size", "Label", 24);
+        theme.SetFontSize("font_size", "Button", 24);
+        doNotAskAgainCheckBox.SetTheme(theme);
         doNotAskAgainCheckBox.Text = "Do not ask again?";
         doNotAskAgainCheckBox.Pressed += OnDoNotAskAgainCheckBoxPressed;
+        this.SetTheme(theme);
         this.AddChild(doNotAskAgainCheckBox);
         this.SetTitle("Exit Game Confirmation");
 		this.SetSize(new Vector2I(300, 150));
@@ -26,16 +28,6 @@ public partial class ExitConfirmationDialog : ConfirmationDialog
     {
         // Save the state of the checkbox
     }
-    public ExitConfirmationDialog()
-    {
-        this.SetTitle("Exit Game Confirmation");
-        this.SetTitle("Exit Game Confirmation");
-		this.SetSize(new Vector2I(300, 150));
-		this.DialogText = "Are you sure you want to exit the game?";
-		this.GetCancelButton().Pressed += OnCancelExitGame;
-		this.GetOkButton().Pressed += OnExitGameConfirmed;
-    }
-
     private void OnExitGameConfirmed()
     {
         GetTree().Quit();
