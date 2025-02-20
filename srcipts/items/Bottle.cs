4e6af03936d533456e4a3d6b3ebcaae8e20081d5
Michał Wojewoda <michal.wojewoda@hotmail.com>
2025-02-20 19:24:13 +0100

    Add bottle scene and script

---
+++ srcipts/items/Bottle.cs
@@ -0,0 +1,58 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+[Tool]
+public partial class Bottle : Node2D
+{
+	private AnimatedSprite2D _animatedSprite;
+
+	public enum BottleColor
+	{
+		Green,
+		Orange,
+		Pink,
+		Blue
+	}
+
+	[Export]
+	public BottleColor Color
+	{
+		get => _color;
+		set
+		{
+			_color = value;
+			UpdateSpriteColor();
+		}
+	}
+	private BottleColor _color = BottleColor.Green;
+
+	// Mapping enum values to frame numbers
+	private readonly Dictionary<BottleColor, int> _colorFrameMap = new Dictionary<BottleColor, int>
+	{
+		{ BottleColor.Green, 0 },
+		{ BottleColor.Orange, 1 },
+		{ BottleColor.Pink, 2 },
+		{ BottleColor.Blue, 3 }
+	};
+
+
+	public override void _Ready()
+	{
+		_animatedSprite = GetNode<AnimatedSprite2D>("BottleSprite");
+		UpdateSpriteColor();
+	}
+
+	private void UpdateSpriteColor()
+	{
+		if (_colorFrameMap.TryGetValue(Color, out int frame))
+		{
+			_animatedSprite.Frame = frame;
+			GD.Print($"Frame set to {frame}");
+		}
+		else
+		{
+			GD.PrintErr($"No frame mapped for color: {Color}");
+		}
+	}
+}
