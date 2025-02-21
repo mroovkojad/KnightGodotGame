1d7c5a8aaf23816ad1d3e2e97bac4a8543f95707
Michał Wojewoda <michal.wojewoda@hotmail.com>
2025-02-21 23:03:47 +0100

    restructure menu logic

--- srcipts/Menus/MainMenu.cs
+++ srcipts/Menus/MainMenu.cs
@@ -10,12 +10,31 @@ public partial class MainMenu : Control
 	SceneManager sceneManager; public override void _Ready()
 	{
 		sceneManager = (SceneManager)GetNode<SceneManager>("/root/SceneManager");
-		startButton = GetNode<Button>("Start");
-		optionsButton = GetNode<Button>("Options");
-		exitButton = GetNode<Button>("ExitGame");
-		startButton.Connect("pressed", new Callable(this, nameof(OnStartPressed)));
-		optionsButton.Connect("pressed", new Callable(this, nameof(OnOptionsPressed)));
-		exitButton.Connect("pressed", new Callable(this, nameof(OnExitGamePressed)));
+		VBoxContainer vbox = GetNode<VBoxContainer>("VBoxContainer2/CenterContainer/VBoxContainer");
+		startButton = vbox.GetNode<Button>("Start");
+		optionsButton = vbox.GetNode<Button>("Options");
+		exitButton = vbox.GetNode<Button>("ExitGame");
+
+		ConnectButtonSignals();
+	}
+
+	private void ConnectButtonSignals()
+	{
+		if (!startButton.IsConnected("pressed", new Callable(this, nameof(OnStartPressed))))
+		{
+			startButton.Connect("pressed", new Callable(this, nameof(OnStartPressed)));
+			GD.Print("Connected start button");
+		}
+		if (!optionsButton.IsConnected("pressed", new Callable(this, nameof(OnOptionsPressed))))
+		{
+			optionsButton.Connect("pressed", new Callable(this, nameof(OnOptionsPressed)));
+			GD.Print("Connected options button");
+		}
+		if (!exitButton.IsConnected("pressed", new Callable(this, nameof(OnExitGamePressed))))
+		{
+			exitButton.Connect("pressed", new Callable(this, nameof(OnExitGamePressed)));
+			GD.Print("Connected exit button");
+		}
 	}
 	private void OnStartPressed()
 	{
@@ -24,7 +43,7 @@ public partial class MainMenu : Control
 
 	private void OnOptionsPressed()
 	{
-		SceneManager.instance.ChangeScene(MainScenes.Options);
+		sceneManager.ChangeScene(MainScenes.Options);
 	}
 
 	private void ShowExitConfirmationDialog()
--- srcipts/SceneManager.cs
+++ srcipts/SceneManager.cs
@@ -1,54 +1,72 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+
 // Enum to represent the main scenes of the game, that can be used as main view.
 public enum MainScenes
-    {
-        MainMenu,
-        Game,
-        Options,
-    }
+{
+    MainMenu,
+    Game,
+    Options,
+}
+
 public partial class SceneManager : Node
 {
     // Dictionary to store the paths of the main scenes.
     Dictionary<MainScenes, string> ScenePaths = new Dictionary<MainScenes, string> {
-    { MainScenes.MainMenu, "res://Scenes/Menu/MainMenu.tscn" },
-    { MainScenes.Game, "res://Scenes/Game.tscn" },
-    { MainScenes.Options, "res://Scenes/Menu/OptionsMenu.tscn" },
-};
+        { MainScenes.MainMenu, "res://scenes/menus/mainMenu.tscn" },
+        { MainScenes.Game,     "res://scenes/game.tscn" },
+        { MainScenes.Options,  "res://scenes/menus/optionsMenu.tscn" },
+    };
+
     public Node CurrentScene { get; set; }
     public static SceneManager instance;
 
     public override void _Ready()
     {
         Viewport root = GetTree().Root;
-        CurrentScene = root.GetChild(-1);
+        CurrentScene = root.GetChild(root.GetChildCount() - 1);
         instance = this;
     }
-    public  void ChangeScene(MainScenes sceneEnum)
+
+    public void ChangeScene(MainScenes sceneEnum)
     {
         CallDeferred(MethodName.DeferredChangeScene,  Variant.From(sceneEnum));
     }
-    public  void DeferredChangeScene(MainScenes sceneEnum)
+
+    public void DeferredChangeScene(MainScenes sceneEnum)
     {
         // It is now safe to remove the current scene.
         try
         {
-            CurrentScene.Free();
+            if (CurrentScene != null)
+            {
+                CurrentScene.Free();
+            }
         }
         catch (Exception ex)
         {
             GD.PrintErr("Error freeing current scene: " + ex.Message);
         }
+
         // Get the path to the scene from the dictionary.
         string path = ScenePaths[sceneEnum];
         // Load a new scene.
         var nextScene = GD.Load<PackedScene>(path);
+        GD.Print("Loaded path: " + path);
+
+        // Check if the scene was loaded successfully
+        if (nextScene == null)
+        {
+            GD.PrintErr("Failed to load scene: " + path);
+            return;
+        }
+
         // Instance the new scene.
         CurrentScene = nextScene.Instantiate();
         // Add it to the active scene, as child of root.
         GetTree().Root.AddChild(CurrentScene);
         // Optionally, to make it compatible with the SceneTree.change_scene_to_file() API.
         GetTree().CurrentScene = CurrentScene;
-        }
+    }
 }
